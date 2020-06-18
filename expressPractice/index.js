e831ad9c343c8081f0ee3d01789525c86c1577df
jeffeggink <jpeggink@gmail.com>
2020-06-18 11:14:46 -0700

    loggerModule

--- expressPractice/index.js
+++ expressPractice/index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const path = require('path');
-const moment = require('moment');
+const logger = require('./middleWaree/logger');
 const members = require('./public/Members')
 
 const app = express();
-
-const logger = (req, res, next) =>  {
-console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`
-);
-    next();
-}
 
 app.use(logger);
 
