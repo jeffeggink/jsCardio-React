6ada33cf54953739763fdcc1f1b14dac8873d921
jeffeggink <jpeggink@gmail.com>
2020-06-18 09:57:34 -0700

    nodeMON

--- expressPractice/index.js
+++ expressPractice/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Helllz yeeahh</h1>');
+    res.send('<h1>Helllz yeeahh mfr</h1>');
 });
 
 const PORT = process.env.PORT || 5000;
