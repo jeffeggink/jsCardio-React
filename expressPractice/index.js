f725763a9f160d978bfec95747eacf8f807e0a34
jeffeggink <jpeggink@gmail.com>
2020-06-18 12:04:49 -0700

    routes

--- expressPractice/index.js
+++ expressPractice/index.js
@@ -1,28 +1,15 @@
 const express = require('express');
 const path = require('path');
 const logger = require('./middleWaree/logger');
-const members = require('./public/Members')
 
 const app = express();
 
-app.use(logger);
-
-//gets all members
-app.get('/api/members', (req, res) =>  res.json(members));
-
-//gets one member
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
-    if (found) {
-        res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({msg: `no member with that ${req.params.id} id  you idiot`})
-    }
-    //res.json(members.filter(member => member.id === parseInt(req.params.id)));
-});
-
 //sets public folder to static 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('api/members', require('./routes/api/members'));
+
+//app.use('/api/members')
 
 const PORT = process.env.PORT || 5000;
 
---
+++ expressPractice/routes/api/members.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const members = require('../../public/Members');
+
+//gets all members
+router.get('/', (req, res) =>  res.json(members));
+
+//gets one member
+router.get('/id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({msg: `no member with that ${req.params.id} id  you idiot`})
+    }
+});
+
+module.exports = router;
