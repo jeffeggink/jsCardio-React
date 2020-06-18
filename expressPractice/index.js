e43545cf45efd5105831c23d7dd390abea15dadb
jeffeggink <jpeggink@gmail.com>
2020-06-18 10:22:57 -0700

    postmanListMembers

--- expressPractice/index.js
+++ expressPractice/index.js
@@ -3,6 +3,31 @@ const path = require('path');
 
 const app = express();
 
+const members = [
+    {
+        id: 1,
+        name: 'joe doe',
+        email: 'cat@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'jane doe',
+        email: 'rat@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'kevin jerk',
+        email: 'bat@gmail.com',
+        status: 'active'
+    }
+]
+
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
