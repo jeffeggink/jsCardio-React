f17b70006e46fd5d19fb028247c9388d046bd702
jeffeggink <jpeggink@gmail.com>
2020-06-18 11:32:05 -0700

    handleBadReq

--- expressPractice/index.js
+++ expressPractice/index.js
@@ -8,9 +8,19 @@ const app = express();
 app.use(logger);
 
 //gets all members
-app.get('/api/members', (req, res) => {
-    res.json(members);
+app.get('/api/members', (req, res) =>  res.json(members));
+
+//gets one member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({msg: `no member with that ${req.params.id} id  you idiot`})
+    }
+    //res.json(members.filter(member => member.id === parseInt(req.params.id)));
 });
+
 //sets public folder to static 
 app.use(express.static(path.join(__dirname, 'public')));
 
