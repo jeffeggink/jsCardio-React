2f7315bd5b5a8bd375bc306da96087868bab742d
jeffeggink <jpeggink@gmail.com>
2020-06-25 20:02:08 -0700

    stopLight

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-const posts = [
-    {title: 'Post One', body: 'this is post one'}
-    {title: 'Post Two', body: 'this is post two'}
-];
+document.getElementById('stopButton').onclick = illuminateRed;
+document.getElementById('slowButton').onclick = illuminateYellow;
 
-function getPosts() {
-    setTimeout(() => {
-        let output = '';
-        posts.forEach((post, index) => {
-        output += `<li>${post}</li>`;
-        });
-        document.body.innerHTML = output;
-    }, 1000);
+function illuminateRed() {
+  clearLights();
+  document.getElementById('stopLight').style.backgroundColor = "red";
 }
 
-createPost(post){
-    setTimeout(() => {
-    posts.push(post);
-    }, 2000);
+function illuminateYellow() {
+  clearLights();
+  document.getElementById('slowLight').style.backgroundColor = "purple";
 }
 
-getPosts();
-
-createPost({
-    title: 'post three', body: 'this is post three'
-})
-.then(getPosts);
-.catch(err => console.log(err));
-
-const promise1 = promise.resolve('hello mfr');
-const promise2 = 10;
-const promise3 = new Promise([promise1, promise2, promise3]).then(values => console.log(values));
+function clearLights() {
+  document.getElementById('stopLight').style.backgroundColor = "black";
+  document.getElementById('slowLight').style.backgroundColor = "black";
+  document.getElementById('goLight').style.backgroundColor = "black";
+}
